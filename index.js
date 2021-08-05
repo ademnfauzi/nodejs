e1636943f0f5d3b06b31497f709750cdeab25b50
Ade Muhammad Nur Fauzi <ademnfauzi76@gmail.com>
2021-08-06 05:35:55 +0700

    git init

---
+++ index.js
@@ -0,0 +1,7 @@
+// const nama = "ademnfauzi";
+
+const cetakNama = require("./coba");
+
+require("./coba");
+// console.log("Hello index");
+console.log(cetakNama("ademnfauzi"));
---
+++ test/satu.js
@@ -0,0 +1,6 @@
+// const nama = "ademnfauzi";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+// console.log(window);
+
+console.log(cetakNama("ademnfauzi"));
